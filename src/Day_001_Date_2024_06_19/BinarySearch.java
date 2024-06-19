260086298f6b505fc4299403393fbe74a935d101
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2024-06-19 16:44:09 +0530

    this is my first few programs

---
+++ src/Day_001_Date_2024_06_19/BinarySearch.java
@@ -0,0 +1,7 @@
+package Day_001_Date_2024_06_19;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Day_001_Date_2024_06_19/OrderAgnosticBinarySearch.java
@@ -0,0 +1,12 @@
+package Day_001_Date_2024_06_19;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        int[] nums= {};
+        System.out.println(binarySearch(nums));
+    }
+    public static int binarySearch(int[] nums){
+
+        return -1;
+    }
+}
