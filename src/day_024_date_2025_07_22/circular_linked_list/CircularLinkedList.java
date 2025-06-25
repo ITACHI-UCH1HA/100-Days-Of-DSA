a54f67e7b2f22c8aaa69fe3cd8daaeaad5f05ca3
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-06-25 19:43:22 +0530

    practice done on 25th june 2025

---
+++ src/day_024_date_2025_07_22/circular_linked_list/CircularLinkedList.java
@@ -0,0 +1,53 @@
+package day_024_date_2025_07_22.circular_linked_list;
+
+class Node{
+    Node next;
+    int data;
+
+    Node(int data){
+        this.data =data;
+        this.next = null;
+    }
+}
+public class CircularLinkedList {
+    Node head;
+    CircularLinkedList(){
+        head = null;
+    }
+    CircularLinkedList(Node node){
+        head = node;
+        head.next=head;
+    }
+    public Node addFirst(int data){
+        Node temp = new Node(data);
+        if(head==null){
+            head = temp;
+            head.next=temp;
+        }else{
+            temp.next = head.next;
+            head.next = temp;
+            int curr= head.data;
+            head.data = temp.data;
+            temp.data = curr;
+        }
+
+
+        return head;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("NULL");
+        }else {
+            do{
+                System.out.print(head.data);
+                head=head.next;
+            }while(head.next!=head);
+        }
+    }
+    public static void main(String[] args) {
+        CircularLinkedList list = new CircularLinkedList();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.print();
+    }
+}
