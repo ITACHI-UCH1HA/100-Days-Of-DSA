d480083a50e619e55460ff194d6f854a34a5d735
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-07-03 23:24:10 +0530

    all the practice done on 3rd july 2025

--- src/day_025_date_2025_08_02/recursion/ConvertIntegerToTheSumOfTwoNoZeroIntegers.java
+++ src/day_025_date_2025_08_02/recursion/ConvertIntegerToTheSumOfTwoNoZeroIntegers.java
@@ -1,2 +1,19 @@
-package day_025_date_2025_08_02.recursion;public class ConvertIntegerToTheSumOfTwoNoZeroIntegers {
+package day_025_date_2025_08_02.recursion;
+
+import java.util.Arrays;
+
+public class ConvertIntegerToTheSumOfTwoNoZeroIntegers {
+    public static void main(String[] args) {
+        int n = 21222;
+        System.out.println(Arrays.toString(getNoZeroIntegers(n)));
+    }
+    public static int[] getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n-a;
+            if(!String.valueOf(a).contains("0") && !String.valueOf(b).contains("0")){
+                return new int[]{a,b};
+            }
+        }
+        return new int[0];
+    }
 }
